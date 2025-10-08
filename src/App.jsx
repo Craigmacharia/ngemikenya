0789bd317e491a18935b9d7c24b8ff3cfc57033b
craigmacharia <cmacharia482@gmail.com>
2025-10-08 12:34:59 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Post from "./pages/Post";
+import Home from "./Pages/Home.jsx";
+import Post from "./Pages/Post.jsx";
+
+
 
 function App() {
   return (
