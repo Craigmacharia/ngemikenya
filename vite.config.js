d0e8b3f8506518f961cef6ddf330372cd9550f53
craigmacharia <cmacharia482@gmail.com>
2025-10-14 01:30:21 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.md'], // this is fine for assets
-  optimizeDeps: {
-    exclude: ['.md'], // ✅ single pattern instead of **/*.md
-  },
-  build: {
-    rollupOptions: {
-      external: [/\.md$/], // ensures Vite doesn’t parse markdown as JS
-    },
-  },
+  assetsInclude: ['**/*.md']
 })
