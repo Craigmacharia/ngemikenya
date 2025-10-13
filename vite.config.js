6ffce9366b02a05ec9262e8970c37dd1ac9a67cf
craigmacharia <cmacharia482@gmail.com>
2025-10-13 23:48:39 +0300

    highbreed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'], // this is fine for assets
+  optimizeDeps: {
+    exclude: ['.md'], // ✅ single pattern instead of **/*.md
+  },
+  build: {
+    rollupOptions: {
+      external: [/\.md$/], // ensures Vite doesn’t parse markdown as JS
+    },
+  },
 })
-
-
