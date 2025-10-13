66eac5fa553d57adba50356e39583e7b46f64f49
craigmacharia <cmacharia482@gmail.com>
2025-10-14 02:42:36 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Post from "./pages/Post";
-import Posts from "./pages/Posts";
+import Posts from "./Pages/Posts";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
