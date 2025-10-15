fcbb7e49f5f3c0a1ea7aa8504516ff1fe91c8f03
craigmacharia <cmacharia482@gmail.com>
2025-10-15 22:55:02 +0300

    Moved all pages to components and fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components (all from components folder)
 import Home from "./components/Home";
+import Posts from "./components/Posts";
 import Post from "./components/Post";
-import Posts from "./pages/Posts";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/post/:slug" element={<Post />} />
-      </Routes>
-      <Footer />
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/post/:slug" element={<Post />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
 
 export default App;
+
 
