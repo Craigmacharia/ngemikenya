f8ec5bbc30652b24c03c86ed2c1d46b300e845a3
craigmacharia <cmacharia482@gmail.com>
2025-10-15 22:33:23 +0300

    moded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./components/Home";
 import Post from "./pages/Post";
 import Posts from "./pages/Posts";
 import Navbar from "./components/Navbar";
